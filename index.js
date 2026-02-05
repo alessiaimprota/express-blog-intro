82932bf2ee873beef55d70b25edc76d9bf48563f
alessiaimprota <alessiaimprota126@gmail.com>
2026-02-05 17:28:39 +0100

    move ooon

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("provaaaa!");
+});
+
+app.listen(port, () => {
+  console.log("ci sei");
 });
