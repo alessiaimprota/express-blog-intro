f37901e3dd8dbea05d54a71d28b5cb245501e704
alessiaimprota <alessiaimprota126@gmail.com>
2026-02-05 17:38:00 +0100

    prove

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const posts = [
+  {
+    id: 1,
+    title: "ciambellone",
+    img: "public/images/ciambellone.jpeg",
+  },
+  {
+    id: 2,
+    title: "cracker barbabietola",
+    img: "public/images/cracker_barbabietola.jpeg",
+  },
+];
+
+console.log(posts);
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("provaaaa!");
+  res.json(posts);
 });
 
 app.listen(port, () => {
